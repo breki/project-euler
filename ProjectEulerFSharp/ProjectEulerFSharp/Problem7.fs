a686da1d9a9c5ff87ad929ab43de7a593a69779c
Igor Brejc <igor.brejc@gmail.com>
2024-12-06 15:43:20 +0100

    Problem 7

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem7.fs
@@ -0,0 +1,21 @@
+﻿module Problem7
+
+open Problems
+open Xunit
+open Swensen.Unquote
+
+let findNthPrime n =
+    let mutable m = 1L
+    let mutable primes = []
+
+    while primes |> List.length < n do
+        let m', primes' = nextPrime (m, primes)
+        m <- m'
+        primes <- primes'
+
+    primes |> List.head
+
+[<Fact>]
+let ``Problem 7`` () =
+    test <@ findNthPrime 6 = 13 @>
+    test <@ findNthPrime 10001 = 104743 @>
