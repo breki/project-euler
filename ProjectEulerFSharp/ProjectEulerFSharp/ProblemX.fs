8c50dbbd0bb3e4902ac16002782a05066d2fb58c
Igor Brejc <igor.brejc@gmail.com>
2024-12-08 08:45:04 +0100

    Problem 10: a naive implementation that is too slow as a solution

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/ProblemX.fs
@@ -0,0 +1,20 @@
+﻿module ProblemX
+
+open Problems
+open Xunit
+open Xunit.Abstractions
+
+
+type Problems(output: ITestOutputHelper) =
+    [<Fact>]
+    member this.``Problem X``() =
+        let max = 100_000
+
+        let primes = findPrimesUpTo max
+        let sum = primes |> List.sum
+
+        output.WriteLine(
+            sprintf "Number of primes below %d: %d" max (primes |> List.length)
+        )
+
+        output.WriteLine(sprintf "Sum of primes below %d: %d" max sum)
