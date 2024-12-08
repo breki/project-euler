0746fcbeb1b2ffcc0151811b019234b96516bf5f
Igor Brejc <igor.brejc@gmail.com>
2024-12-08 08:37:53 +0100

    Problem 9: again, a brute-force approach

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem9.fs
@@ -0,0 +1,34 @@
+﻿module Problem9
+
+open Xunit
+open Xunit.Abstractions
+
+
+let findAB () =
+    let findA a =
+        [ a + 1 .. 1000 ]
+        |> List.tryFind (fun b ->
+            let c = 1000 - a - b
+            a * a + b * b = c * c)
+
+    [ 1..1000 ]
+    |> List.map (fun a -> (a, findA a))
+    |> List.filter (fun (_, b) -> b |> Option.isSome)
+    |> List.map (fun (a, b) -> (a, b.Value))
+
+type Problems(output: ITestOutputHelper) =
+    /// <summary>
+    /// Special Pythagorean Triplet
+    /// </summary>
+    [<Fact>]
+    member this.``Problem 9``() =
+        let candidates = findAB ()
+
+        candidates
+        |> List.iter (fun (a, b) ->
+            let c = 1000 - a - b
+            Assert.Equal(a * a + b * b, c * c)
+            output.WriteLine(sprintf "%d² + %d² = %d²" a b c)
+
+            let product = a * b * c
+            output.WriteLine(sprintf "Product: %d" product))
