826ea8f09d4528295a6ff4829f5f2f3200e2b407
Igor Brejc <igor.brejc@gmail.com>
2024-12-12 06:39:35 +0100

    Trying to fix divisors()

--- ProjectEulerFSharp/ProjectEulerFSharp/ProblemX.fs
+++ ProjectEulerFSharp/ProjectEulerFSharp/ProblemX.fs
@@ -1,38 +1,45 @@
 ﻿module ProblemX
 
+open System
 open Xunit
 open Swensen.Unquote
 open Problem10
+open Xunit.Abstractions
 
 type TD = { tn: int; ds: int list }
 
-let triangleNumbers n : int list =
-    let findNext (numbersSoFar: int list) (n: int) =
+let triangleNumbers (n: int64) : int64 list =
+    let findNext (numbersSoFar: int64 list) (n: int64) =
         let m = numbersSoFar |> List.head
         let o = m + n
         o :: numbersSoFar
 
-    [ 2..n ] |> List.fold findNext [ 1 ]
+    [ 2L .. n ] |> List.fold findNext [ 1L ]
 
-let sieve = sieveOfEratosthenes 1000
+let sieve = sieveOfEratosthenes 1000000
 
-let findSmallestPrimeDivisor n =
-    let mutable m = 2
+let findSmallestPrimeDivisor (n: int64) =
+    let mutable m = 2L
     let mutable divisor = None
 
     while divisor.IsNone do
-        match sieve[m] with
+        if m >= (Array.length sieve) || m >= Int32.MaxValue then
+            sprintf "No divisor found for %d" n |> failwith
+        else
+            ()
+
+        match sieve[int32 m] with
         | 1uy -> if n % m = 0 then divisor <- Some m else ()
         | _ -> ()
 
-        m <- m + 1
+        m <- m + 1L
 
     divisor |> Option.get
 
-let divisors n : Set<int> =
-    let rec div' (d: Set<int>) n : Set<int> =
+let divisors (n: int64) : Set<int64> =
+    let rec div' (d: Set<int64>) (n: int64) : Set<int64> =
         match n with
-        | 1 -> d |> Set.add 1
+        | 1L -> d |> Set.add 1
         | n ->
             let primeDivisor = findSmallestPrimeDivisor n
 
@@ -42,14 +49,14 @@ let divisors n : Set<int> =
 
             while remaining % primeDivisor = 0 do
                 remaining <- remaining / primeDivisor
-                d' <- Set.union d' (set [ divisor; remaining ])
+                d' <- (d' |> Set.add divisor) |> Set.union (div' d' remaining)
                 divisor <- divisor * primeDivisor
 
-            div' d' remaining
+            div' (d' |> Set.add n) remaining
 
-    div' Set.empty n |> Set.add n
+    div' Set.empty n
 
-type Problems() =
+type Problems(output: ITestOutputHelper) =
     [<Fact>]
     member this.``Find triangle numbers``() =
         test <@ triangleNumbers 1 = [ 1 ] @>
@@ -67,3 +74,21 @@ type Problems() =
         test <@ divisors 15 = set [ 1; 3; 5; 15 ] @>
         test <@ divisors 21 = set [ 1; 3; 7; 21 ] @>
         test <@ divisors 28 = set [ 1; 2; 4; 7; 14; 28 ] @>
+
+        test
+            <@
+                divisors 120 = set
+                    [ 1; 2; 3; 4; 5; 6; 8; 10; 12; 15; 20; 24; 30; 40; 60; 120 ]
+            @>
+
+    [<Fact(Skip = "Skip until divisors() works properly")>]
+    // [<Fact>]
+    member this.``ProblemX``() =
+        triangleNumbers 100
+        |> List.rev
+        |> List.map (fun t ->
+            let d = divisors t
+            (t, Set.count d, d))
+        |> List.filter (fun (_, c, _) -> c > 5)
+        |> List.iter (fun (t, c, ds) ->
+            output.WriteLine(sprintf "%d: %d - %A" t c ds))
