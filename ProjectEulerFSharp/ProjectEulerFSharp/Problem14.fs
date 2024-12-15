72246c5fe62a8054f0a66fc8828b0d3ce9e277cd
Igor Brejc <igor.brejc@gmail.com>
2024-12-15 07:27:21 +0100

    Problem 14: initial, naive solution

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem14.fs
@@ -0,0 +1,38 @@
+﻿module Problem14
+
+open Xunit
+open Swensen.Unquote
+open Xunit.Abstractions
+
+let collatzSequence n =
+    let rec c' s n =
+        match n with
+        | 1 -> 1 :: s
+        | _ ->
+            let m =
+                if n % 2 = 0 then
+                    n / 2
+                else
+                    3 * n + 1
+
+            c' (n :: s) m
+
+    (c' [] n) |> List.rev
+
+
+type Problems(_output: ITestOutputHelper) =
+    [<Fact>]
+    member this.``Collatz sequence``() =
+        test <@ collatzSequence 13 = [ 13; 40; 20; 10; 5; 16; 8; 4; 2; 1 ] @>
+
+    [<Fact>]
+    member this.``Longest Collatz sequence``() =
+        let cs =
+            [1 .. 100000]
+            |> List.map(fun n -> collatzSequence n |> List.length)
+            |> List.sort
+            |> List.rev
+
+        let longest = cs |> List.head
+        test <@ longest = 351  @>
+
