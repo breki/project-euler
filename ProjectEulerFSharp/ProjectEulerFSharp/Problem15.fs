d6d6ba41698242efee2a053006e3c357c944b8d1
Igor Brejc <igor.brejc@gmail.com>
2024-12-17 17:02:40 +0100

    Problem 15

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem15.fs
@@ -0,0 +1,22 @@
+﻿module Problem15
+
+open System.Numerics
+open Xunit
+open Swensen.Unquote
+open Xunit.Abstractions
+
+let factorial n =
+    [ 2 .. n ]
+    |> List.map(fun x -> BigInteger(x))
+    |> List.fold (fun product x -> product * x) BigInteger.One
+
+type Problems(_output: ITestOutputHelper) =
+    [<Fact>]
+    member this.``Lattice paths``() =
+        let n = 20
+
+        let routesCount = (factorial (n * 2)) / (factorial n * factorial n )
+        let routesCountStr = routesCount.ToString()
+
+        test <@ routesCountStr = "137846528820" @>
+
