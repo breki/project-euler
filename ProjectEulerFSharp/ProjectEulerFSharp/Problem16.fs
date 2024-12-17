fdf8f8f4c0aaba9ed504cc98552a79d6d08fc12d
Igor Brejc <igor.brejc@gmail.com>
2024-12-17 17:32:18 +0100

    Problem 16

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem16.fs
@@ -0,0 +1,30 @@
+﻿module Problem16
+
+open System.Numerics
+open Xunit
+open Swensen.Unquote
+open Xunit.Abstractions
+
+let powerDigitSum n =
+    let bigInt2 = BigInteger.Parse("2")
+
+    let number = [ 1..n ] |> List.fold (fun p _ -> p * bigInt2) BigInteger.One
+
+
+    let numberStr = number.ToString()
+
+    numberStr
+    |> Seq.toArray
+    |> Array.fold
+        (fun sum c ->
+            let digit = int (c - '0')
+            sum + digit)
+        0
+
+type Problems(_output: ITestOutputHelper) =
+    [<Fact>]
+    member this.``Power digit sum``() =
+        test <@ powerDigitSum 0 = 1 @>
+        test <@ powerDigitSum 1 = 2 @>
+        test <@ powerDigitSum 15 = 26 @>
+        test <@ powerDigitSum 1000 = 1366 @>
