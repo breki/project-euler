4ea4f4d6c6aa8ad06e7ab9b6d88c2e8ea20f1742
Igor Brejc <igor.brejc@gmail.com>
2024-12-24 06:31:53 +0100

    Problem 24

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem24.fs
@@ -0,0 +1,38 @@
+﻿module Problem24
+
+open Xunit
+open Swensen.Unquote
+open Xunit.Abstractions
+
+let rec lexicographicPermutations
+    (digitsAvailable: string)
+    (numberSoFar: string)
+    =
+    if digitsAvailable.Length > 0 then
+        [| 0 .. digitsAvailable.Length - 1 |]
+        |> Array.map (fun i ->
+            let t' = numberSoFar + digitsAvailable[i].ToString()
+            let n' = digitsAvailable.Remove(i, 1)
+            let x = lexicographicPermutations n' t'
+            x)
+        |> Array.concat
+    else
+        [| numberSoFar |]
+
+type Problems(_output: ITestOutputHelper) =
+    [<Fact>]
+    member this.``lexicographic permutations``() =
+        test
+            <@
+                lexicographicPermutations "012" "" = [| "012"
+                                                        "021"
+                                                        "102"
+                                                        "120"
+                                                        "201"
+                                                        "210" |]
+            @>
+
+        let x = lexicographicPermutations "0123456789" ""
+
+        test <@ x |> Array.length = 3628800 @>
+        test <@ x[1_000_000 - 1] = "2783915460" @>
