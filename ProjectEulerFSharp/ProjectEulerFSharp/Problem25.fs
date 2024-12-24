efb78e9c630b952e14e899bff8db37afca234ee5
Igor Brejc <igor.brejc@gmail.com>
2024-12-24 07:13:03 +0100

    Problem 25

---
+++ ProjectEulerFSharp/ProjectEulerFSharp/Problem25.fs
@@ -0,0 +1,35 @@
+﻿module Problem25
+
+open System.Numerics
+open Xunit
+open Swensen.Unquote
+open Xunit.Abstractions
+
+let bigIntegerDigitsCount (n: BigInteger) = n.ToString().Length
+
+let indexOfFirstFibonacciNumberWithDigitsCount l =
+    match l with
+    | 0 -> 1
+    | 1 -> 1
+    | l ->
+            let mutable f = [| BigInteger.One; BigInteger.One |]
+
+            Seq.initInfinite (
+                fun i ->
+                    let m = f[0] + f[1]
+                    f[0] <- f[1]
+                    f[1] <- m
+                    m |> bigIntegerDigitsCount
+                    )
+            |> Seq.findIndex (fun x -> x >= l)
+            |> (+) 3
+
+
+type Problems(_output: ITestOutputHelper) =
+    [<Fact>]
+    member this.``1000-digit Fibonacci number``() =
+        test <@ indexOfFirstFibonacciNumberWithDigitsCount 1 = 1 @>
+        test <@ indexOfFirstFibonacciNumberWithDigitsCount 2 = 7 @>
+        test <@ indexOfFirstFibonacciNumberWithDigitsCount 3 = 12 @>
+        test <@ indexOfFirstFibonacciNumberWithDigitsCount 10 = 45 @>
+        test <@ indexOfFirstFibonacciNumberWithDigitsCount 1000 = 4782 @>
